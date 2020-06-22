70bb80f6c304698d0d6e7c1c63eceb3d1badb648
Tomi Leppänen <tomi.leppanen@jolla.com>
2020-06-22 17:07:17 +0300

    [sailfish-version] Don't use shopt. Contributes to JB#39735
    Don't use shopt as it's only supported on bash. Checking for login_shell
    shouldn't be needed in /etc/profile.d/ scripts anyway because they are
    only run on login shells.
    
    Signed-off-by: Tomi Leppänen <tomi.leppanen@jolla.com>

--- sailfish-version.sh
+++ sailfish-version.sh
@@ -1,10 +1,8 @@
-if shopt -q login_shell; then
-    if [ -f /etc/os-release ]; then
-        . /etc/os-release
-        echo ",---"
-        echo "| $PRETTY_NAME"
-        echo "'---"
-    else
-        echo "Arrr, you're screwed, mate"
-    fi
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    echo ",---"
+    echo "| $PRETTY_NAME"
+    echo "'---"
+else
+    echo "Arrr, you're screwed, mate"
 fi
